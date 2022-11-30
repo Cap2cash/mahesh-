07b279975429de607f5854b0dc1c287bb771148a
techvky <60184998+techvky@users.noreply.github.com>
2022-11-30 12:48:38 +0530

    Squashed commit of the following:
    commit afef402ed606f84f88db858107cee2d5976f3ebf
    Author: Cap2cash <119475500+Cap2cash@users.noreply.github.com>
    Date:   Wed Nov 30 12:45:00 2022 +0530
    
        Update login.blade.php
    
    commit aa960661babcb093363f2a51c49a73b1dc3e30a1
    Author: Cap2cash <119475500+Cap2cash@users.noreply.github.com>
    Date:   Wed Nov 30 12:42:53 2022 +0530
    
        Update login.blade.php
    
    commit 25fadf515f656235365e02562cd4325858ef7786
    Author: Cap2cash <119475500+Cap2cash@users.noreply.github.com>
    Date:   Wed Nov 30 12:38:09 2022 +0530
    
        Update login.blade.php
    
    commit 99f7ba6e07184401c907481f8f628930e7a97ec0
    Merge: 6390907 f6f01ee
    Author: Cap2cash <119475500+Cap2cash@users.noreply.github.com>
    Date:   Wed Nov 30 12:35:18 2022 +0530
    
        Merge pull request #3 from prabuece/patch-2
    
        Update login.blade.php
    
    commit f6f01ee53a0fa414013b61221d45f869eca3931d
    Author: prabuece <119479405+prabuece@users.noreply.github.com>
    Date:   Wed Nov 30 12:34:08 2022 +0530
    
        Update login.blade.php
    
    commit 63909078bc20f22846ba1b28c9a40d5f98106359
    Author: Cap2cash <119475500+Cap2cash@users.noreply.github.com>
    Date:   Wed Nov 30 12:30:07 2022 +0530
    
        Update login.blade.php
    
    commit 21f48fcb4df0ef343742b2ee97477f956e4e4a0c
    Merge: 66e7f6a fc6a357
    Author: Cap2cash <119475500+Cap2cash@users.noreply.github.com>
    Date:   Wed Nov 30 12:29:16 2022 +0530
    
        Merge pull request #2 from techvky/master
    
        Update web.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -52,7 +52,7 @@
                             </div>
                             <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
                             <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                                <h4 class="mb-4">Welcome to Cap 2 Cash</h4>
+                                <h4 class="mb-4">Welcome Poovarasan bsbjsfbsfbs</h4>
                                 <p class="small mb-0">online money</p>
                             </div>
                             </div>
