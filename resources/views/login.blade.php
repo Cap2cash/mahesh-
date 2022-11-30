d5db8e419d9cae6439c52d6df4ae4d1116cb8fd9
prabuece <119479405+prabuece@users.noreply.github.com>
2022-11-30 13:03:35 +0530

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -53,7 +53,7 @@
                             <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
                             <div class="text-white px-3 py-4 p-md-5 mx-md-4">
                                 <h4 class="mb-4">Welcome Poovarasan Temp</h4>
-                                <p class="small mb-0">online money</p>
+                                <p class="small mb-0"> money</p>
                             </div>
                             </div>
                         </div>
